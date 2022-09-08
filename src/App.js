015429194301249343dfbe2bc30a4f03ad4f98ae
unknown <vladancupric@gmail.com>
2022-09-08 09:41:44 +0200

    Add

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import SignIn from "./pages/Signin";
+import SignUp from "./pages/SignUp";
+import Explore from "./pages/Explore";
+import Profile from "./pages/Profile";
+import ForgotPassword from "./pages/ForgotPassword";
+import Offers from "./pages/Offers";
+
 function App() {
   return (
-  <div className="App">
+    <>
+    <Router>
+    <Routes>
+      <Route path="/"  element={<Explore/>}/>
+      <Route path="/offers"  element={<Offers/>}/>
+      <Route path="/profile"  element={<Profile/>}/>
+      <Route path="/forgotpassword"  element={<ForgotPassword/>}/>
+      <Route path="/signIn"  element={<SignIn/>}/>
+      <Route path="/signUp"  element={<SignUp/>}/>
 
-    <h3>Vladan Cupric</h3>
-  </div>
-  )
+    </Routes>
+    </Router>
+    {/* // NavBar */}
+  </>
+  );
+
 }
 
 export default App;
