14ed503f902f618d8ddd83210817f231692f852b
unknown <vladancupric@gmail.com>
2022-09-09 13:14:53 +0200

    Add

--- src/pages/SignIn.js
+++ src/pages/SignIn.js
@@ -1,11 +1,30 @@
-import React from 'react'
+import React from "react";
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { ReactComponent as ArrowrightIcon } from "../assets/svg/keyboardArrowRightIcon.svg";
+import visibilityIcon from "../assets/svg/visibilityIcon.svg";
 
 const SignIn = () => {
+  const [showPassword, setShowPassword] = useState(false);
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+
+  const { email, password } = formData;
+  const navigate = useNavigate();
   return (
-    <div>
-      <h1>SignIn</h1>
+    <>
+      <div className="pagecontainer">
+        <header>
+          <p className="pageHeader">Welcome Back!</p>
+        </header>
+        <main>
+          
+        </main>
       </div>
-  )
-}
+    </>
+  );
+};
 
 export default SignIn;
