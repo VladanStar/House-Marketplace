c12a332839f888983b02c4897cbdc35424079009
unknown <vladancupric@gmail.com>
2022-09-09 08:12:14 +0200

    Add

--- src/App.js
+++ src/App.js
@@ -1,29 +1,28 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import SignIn from "./pages/Signin";
+import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
 import Explore from "./pages/Explore";
 import Profile from "./pages/Profile";
 import ForgotPassword from "./pages/ForgotPassword";
 import Offers from "./pages/Offers";
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
     <>
-    <Router>
-    <Routes>
-      <Route path="/"  element={<Explore/>}/>
-      <Route path="/offers"  element={<Offers/>}/>
-      <Route path="/profile"  element={<Profile/>}/>
-      <Route path="/forgotpassword"  element={<ForgotPassword/>}/>
-      <Route path="/signIn"  element={<SignIn/>}/>
-      <Route path="/signUp"  element={<SignUp/>}/>
-
-    </Routes>
-    </Router>
-    {/* // NavBar */}
-  </>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Explore />} />
+          <Route path="/offers" element={<Offers />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/signIn" element={<SignIn />} />
+          <Route path="/signUp" element={<SignUp />} />
+          <Route path="/forgot-password" element={<ForgotPassword />} />
+        </Routes>
+      </Router>
+      <NavBar />
+    </>
   );
-
 }
 
 export default App;
